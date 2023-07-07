999055db9a4aef7085136ad1ad99799155cad6b1
Oriol Cervantes <ocervantes@dminc.com>
2023-07-07 15:55:49 +0200

    feat: add line spacing tokens in Android packge

--- packages/tokens/config.js
+++ packages/tokens/config.js
@@ -318,7 +318,7 @@ async function run() {
         files: [
           {
             filter: function(prop) {
-              return (prop.type === 'fontSizes' && prop.filePath !== 'src/global.json' || prop.type === 'lineHeights' && prop.filePath !== 'src/global.json');
+              return (prop.type === 'fontSizes' && prop.filePath !== 'src/global.json' || prop.type === 'lineHeights' && prop.filePath !== 'src/global.json' || prop.type === 'other' && prop.filePath !== 'src/global.json');
             },
             resourceType: "dimen",
             destination: `${mobileDesignTokensFileName}FontDimens.xml`,
